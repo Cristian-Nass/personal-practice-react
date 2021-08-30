c68058e5744e25e9560a2edad6881038a59c08a1
Cristian <cristian.tirdad@gmail.com>
2021-08-30 19:21:33 +0200

    implemented table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
+import "antd/dist/antd.css";
 import CustomSearch from "./components/CustomSearch";
-import PasswordGenerators from "./components/PasswordGenerators";
+// import PasswordGenerators from "./components/PasswordGenerators";
 
 function App() {
   return (
--- src/components/CustomSearch.tsx
+++ src/components/CustomSearch.tsx
@@ -1,11 +1,47 @@
+import React from "react";
+import { Table } from "antd";
 import { users } from "./data";
 
 const CustomSearch = () => {
-  console.log(users);
-  return (
-    <>
-      <div>Salam</div>
-    </>
-  );
+  const columns = [
+    {
+      title: "First Name",
+      dataIndex: "firstName",
+      key: "firstName",
+    },
+    {
+      title: "Last Name",
+      dataIndex: "lastName",
+      key: "firstName",
+    },
+    {
+      title: "Birth",
+      dataIndex: "birthAt",
+      key: "birthAt",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "City",
+      dataIndex: "city",
+      key: "city",
+    },
+    {
+      title: "Adress",
+      dataIndex: "adress",
+      key: "adress",
+    },
+    {
+      title: "Field",
+      dataIndex: "field",
+      key: "field",
+    },
+  ];
+
+  return <Table columns={columns} dataSource={users} />;
 };
+
 export default CustomSearch;
