f7a5d7d4c091b50423b8b00ea22f820ad0b2e4dc
Cristian <cristian.tirdad@gmail.com>
2021-08-30 22:53:37 +0200

    implemented input bpoc for search

--- src/components/customsearch/SelectedSearchItem.tsx
+++ src/components/customsearch/SelectedSearchItem.tsx
@@ -1,11 +1,20 @@
+import { Input } from "antd";
+
 interface SelectedItemsProps {
   selectedItems: string[];
 }
 const SelectedSearchItem = (props: SelectedItemsProps) => {
   console.log(props.selectedItems);
   return (
-    <div style={{ textAlign: "left", paddingTop: "20px" }}>
+    <div style={{ textAlign: "left", paddingTop: "60px" }}>
       <div>Selected Search With</div>
+      {props.selectedItems.map((item, index) => (
+        <Input
+          key={index}
+          placeholder={`Search By ${item}`}
+          style={{ marginTop: "14px" }}
+        />
+      ))}
     </div>
   );
 };
