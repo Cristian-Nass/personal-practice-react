d44d56958cfb589386a16297ac2ac21ddb5b8eb3
Cristian <cristian.tirdad@gmail.com>
2021-09-23 14:06:47 +0200

    Implemented React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import "antd/dist/antd.css";
 import CustomSearch from "./components/customsearch/CustomSearch";
+import Table from "./components/Table";
+import Navbar from "./components/Navbar";
 // import PasswordGenerators from "./components/PasswordGenerators";
 
 function App() {
   return (
     <div className='App'>
       {/* <PasswordGenerators /> */}
-      <CustomSearch />
+      <BrowserRouter>
+        <Navbar />
+        <Route path='/custom-search' component={CustomSearch} />
+        <Route path='/table' component={Table} />
+      </BrowserRouter>
     </div>
   );
 }
